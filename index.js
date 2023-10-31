3b18cc80572d4142cafd08d899ebfe0a3b1b8399
owonikoko Ayomide <donaykoko@yahoo.com>
2023-10-31 11:38:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function bringquad(){
+  document.getElementById('quadratic').style.display="block";
+  document.getElementById('calcutor').style.display="none";
+document.getElementById('btns').innerHTML = '<button title="calculator" ondblclick="bringcalc()"></button>'
+
+
+}
+
+function bringcalc(){
+  document.getElementById('quadratic').style.display="none";
+  document.getElementById('calcutor').style.display="block";
+document.getElementById('btns').innerHTML = '<button title="quadratic" ondblclick="bringquad()"></button>'
+
+
+}
---
+++ quadratic.js
@@ -0,0 +1,26 @@
+function calculate(){
+  var a =  document.getElementById('a').value;
+  var b =  document.getElementById('b').value;
+  var c =  document.getElementById('c').value;
+  // var test= ("-b + sqrt(" + b*b + "- " + 4 * a * c +")/" + 2 *a)
+  // alert(a);
+  var x1 = (-b + Math.sqrt(  
+    (
+      Math.pow(b, 2)- (4 * a * c)
+      
+      )
+    
+    ))/ (2*a);
+  
+    var x2 = (-b - ((Math.sqrt(  
+      (
+        (Math.pow(b, 2))- (4 * a * c)
+        
+        )
+      
+      ))))/ (2*a); 
+  
+    document.getElementById('x1').innerHTML=x1;
+    document.getElementById('x2').innerHTML=x2;
+  
+      }
